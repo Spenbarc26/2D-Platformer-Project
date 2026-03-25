857d5267522a5cb2b0282a94d9b3306489e3fc37
Nathaniel Caley <ncaley2@gmail.com>
2026-03-25 16:47:52 -0400

    Added the Double Jump abilitiy to the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,20 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+    public int extraJumpsValue = 1;
+    public int extraJumps;
 
     private Rigidbody2D rb;
     private bool isGrounded;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        extraJumps = extraJumpsValue;
     }
 
     // Update is called once per frame
@@ -25,10 +31,21 @@ public class Player : MonoBehaviour
 
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if(isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            extraJumps = extraJumpsValue;
         }
+
+        if (Input.GetButtonDown("Jump"))
+            if(isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            else if(extraJumps >0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                extraJumps--;
+            }
     }
 
     private void FixedUpdate()
